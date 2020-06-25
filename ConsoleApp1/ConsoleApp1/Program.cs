11885d11066a04c35a6053c4dac19ae8b1dccd62
Rasel Ahmed <it_rasel@hotmail.com>
2020-06-25 11:19:21 +0600

    commited

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -15,7 +15,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            var connectionString = ConfigurationManager.ConnectionStrings["JulyDB"].ConnectionString;
+            var connectionString = ConfigurationManager.ConnectionStrings["connectionDB"].ConnectionString;
 
             //create instanace of database connection
             SqlConnection conn = new SqlConnection(connectionString);
@@ -31,7 +31,7 @@ namespace ConsoleApp1
 
         private static void phoneCallActivity(SqlConnection conn)
         {
-            string[] filePaths = Directory.GetFiles(@"D:\Rasel Ahmed\UN Files\Phone Call\", "*.*");
+            string[] filePaths = Directory.GetFiles(@"D:\Rasel Ahmed\Files\Phone Call\", "*.*");
             foreach (string filePath in filePaths)
             {
                 Microsoft.Office.Interop.Excel.Application objXL = null;
@@ -96,43 +96,38 @@ namespace ConsoleApp1
                         }
                     }
 
-                    if (ws == objWB.Sheets.Count)
-                    {
-                        System.Data.DataColumn deviceName = new System.Data.DataColumn("devicename", typeof(System.String));
-                        deviceName.DefaultValue = objSHT.Cells[objSHT.UsedRange.Rows.Count, 1].Text;
-                        dt.Columns.Add(deviceName);
-
-                        System.Data.DataColumn dateCreated = new System.Data.DataColumn("activitydate", typeof(System.String));
-                        var dateText = objSHT.Cells[objSHT.UsedRange.Rows.Count, 2].Text;
-                        string activityDate = dateText.Substring(0, dateText.IndexOf("-")).Trim();
-                        dateCreated.DefaultValue = DateTime.Parse(activityDate);
-                        dt.Columns.Add(dateCreated);
-                    }
                 }
 
                 // insert data to database 
                 foreach (DataRow row in dt.Rows)
                 {
-                    string queryString = @"INSERT INTO PhoneSystemImportActivityData 
-                                        (ContactId,	
-                                        AgentId,	
-                                        AgentName,	
-                                        ShiftDuration,	
-                                        ACDCount,	
-                                        ACDTalkTime,	
-                                        NonACDCount,	
-                                        NonACDTalkTime,	
-                                        OutboundCount,	
-                                        OutboundTalkTime,	
-                                        MakeBusyTime,	
-                                        DeviceName,	
-                                        ActivityDate)
-                                        VALUES([dbo].[fnGetContactIdByFullName]('" + row["agentname"].ToString() + "')," + Convert.ToInt32(row["agentid"]) + ", '" + row["agentname"].ToString() + "' " +
-                                        ",'" + row["shiftduration"].ToString() + "' ," + Convert.ToInt32(row["acdcount"]) + "" +
-                                        ",'" + row["acdtalktime"].ToString() + "'," + Convert.ToInt32(row["nonacdcount"]) + "" +
-                                        ",'" + row["nonacdtalktime"].ToString() + "'," + Convert.ToInt32(row["outboundcount"]) + "" +
-                                        ",'" + row["outboundtalktime"].ToString() + "','" + row["makebusytime"].ToString() + "'" +
-                                        ",'" + row["devicename"].ToString() + "','" + row["activitydate"].ToString() + "'" +
+                    string queryString = @"INSERT INTO TableName 
+                                        (column1,	
+                                        column2,	
+                                        column3,	
+                                        column4,	
+                                        column5,	
+                                        column6,	
+                                        column7,	
+                                        column8,	
+                                        column9,	
+                                        column10,	
+                                        column11,	
+                                        column12,	
+                                        column13)
+                                        VALUES('"+ row["column1"].ToString() + "'" +
+                                        "," + Convert.ToInt32(row["column2"]) + "" + // datatype int 
+                                        ", '" + row["column3"].ToString() + "' " +
+                                        ",'" + row["column4"].ToString() + "' " +
+                                        "," + Convert.ToInt32(row["column5"]) + "" +
+                                        ",'" + row["column6"].ToString() + "'" +
+                                        "," + Convert.ToInt32(row["column7"]) + "" +
+                                        ",'" + row["column8"].ToString() + "'" +
+                                        "," + Convert.ToInt32(row["column9"]) + "" +
+                                        ",'" + row["column10"].ToString() + "'" +
+                                        ",'" + row["column11"].ToString() + "'" +
+                                        ",'" + row["column12"].ToString() + "'" +
+                                        ",'" + row["column13"].ToString() + "'" +
                                         ")";
 
 
@@ -150,7 +145,7 @@ namespace ConsoleApp1
 
         private static void EmailActivity(SqlConnection conn)
         {
-            string[] filePaths = Directory.GetFiles(@"D:\Rasel Ahmed\UN Files\Email\", "*.*");
+            string[] filePaths = Directory.GetFiles(@"D:\Rasel Ahmed\Files\Email\", "*.*");
             foreach (string filePath in filePaths)
             {
                 //reading all the lines(rows) from the file.
@@ -179,39 +174,39 @@ namespace ConsoleApp1
                 // save data into database table 
                 foreach (DataRow row in dtData.Rows)
                 {
-                    int isDeleted = row["isdeleted"].ToString().ToLower() == "false" ? 0 : 1;
-                    string queryString = @"INSERT INTO EmailSystemImportActivityData 
-                                            (ContactId
-                                            ,ReportRefreshDate
-                                            ,UserPrincipalName
-                                            ,DisplayName
-                                            ,IsDeleted
-                                            ,DeletedDate
-                                            ,LastActivityDate
-                                            ,SendCount
-                                            ,ReceiveCount
-                                            ,ReadCount
-                                            ,MeetingCreatedCount
-                                            ,MeetingInteractedCount
-                                            ,AssignedProducts
-                                            ,ReportPeriod
-                                            ,InsertedDate
+                    int isDeleted = row["column5"].ToString().ToLower() == "false" ? 0 : 1;
+                    string queryString = @"INSERT INTO TableName 
+                                            (column1
+                                            ,column2
+                                            ,column3
+                                            ,column4
+                                            ,column5
+                                            ,column6
+                                            ,column7
+                                            ,column8
+                                            ,column9
+                                            ,column10
+                                            ,column11
+                                            ,column12
+                                            ,column13
+                                            ,column14
+                                            ,column15
                                         )
                                         
-                                        VALUES([dbo].[fnGetContactIdByFullName]('" + row["displayname"].ToString() + "')" +
-                                            ",'" + row["reportrefreshdate"].ToString() + "'" +
-                                            ",'" + row["userprincipalname"].ToString() + "' " +
-                                            ",'" + row["displayname"].ToString() + "'" +
+                                        VALUES('" + row["column1"].ToString() + "'" +
+                                            ",'" + row["column2"].ToString() + "'" +
+                                            ",'" + row["column3"].ToString() + "' " +
+                                            ",'" + row["column4"].ToString() + "'" +
                                             "," + isDeleted + "" +
-                                            ",'" + row["deleteddate"].ToString() + "'" +
-                                            ",'" + row["lastactivitydate"].ToString() + "'" +
-                                            "," + Convert.ToInt32(row["sendcount"]) + "" +
-                                            "," + Convert.ToInt32(row["receivecount"]) + "" +
-                                            "," + Convert.ToInt32(row["readcount"]) + "" +
-                                            "," + Convert.ToInt32(row["meetingcreatedcount"]) + "" +
-                                            "," + Convert.ToInt32(row["meetinginteractedcount"]) + "" +
-                                            ",'" + row["assignedproducts"].ToString() + "'" +
-                                            "," + Convert.ToInt32(row["reportperiod"]) + "" +
+                                            ",'" + row["column5"].ToString() + "'" +
+                                            ",'" + row["column6"].ToString() + "'" +
+                                            "," + Convert.ToInt32(row["column7"]) + "" +
+                                            "," + Convert.ToInt32(row["column8"]) + "" +
+                                            "," + Convert.ToInt32(row["column9"]) + "" +
+                                            "," + Convert.ToInt32(row["column10"]) + "" +
+                                            "," + Convert.ToInt32(row["column11"]) + "" +
+                                            ",'" + row["column12"].ToString() + "'" +
+                                            "," + Convert.ToInt32(row["column13"]) + "" +
                                             ",getDate()" +
                                         ")";
 
